c8b09b68ca00fcdcbb69cd65b41e8a6059fc95f3
Javier Islas <javier.i@inspireinnovations.com>
2019-05-10 13:33:31 -0500

    Se agrega la primer configuración de la pantalla de login.

--- app/src/main/java/com/mobile/camotero/MainActivity.java
+++ app/src/main/java/com/mobile/camotero/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Se oculta la barra superior para la pantalla de logín.
+        super.getSupportActionBar().hide();
     }
 }
