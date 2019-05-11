c82ad352c73686405fa85a6e69ee80695e28ca27
Javier Islas <javierislas800@gmail.com>
2019-05-11 18:51:01 -0500

    Se agrega el NavigationAction al código.

--- app/src/main/java/com/mobile/camotero/MainActivity.java
+++ app/src/main/java/com/mobile/camotero/MainActivity.java
@@ -1,10 +1,21 @@
 package com.mobile.camotero;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button buttonIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,59 @@ public class MainActivity extends AppCompatActivity {
 
         // Se oculta la barra del action.
         super.getSupportActionBar().hide();
+
+        findViews();
+
+        events();
     }
+
+    private void findViews() {
+        editTextEmail = super.findViewById(R.id.editTextMail);
+        editTextPassword = super.findViewById(R.id.editTextPassword);
+
+        buttonIniciarSesion = super.findViewById(R.id.btnIniciarSesion);
+    }
+
+    private void events() {
+        buttonIniciarSesion.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch(id){
+            case R.id.btnIniciarSesion:
+                doToLogin();
+                break;
+            /*case R.id.textViewGoSignUp:
+                goToSignUp();
+                break;*/
+        }
+    }
+
+
+    private void doToLogin() {
+
+        // Se realiza la validación de correo y password;
+        if (editTextEmail.getText().toString().equals(editTextPassword.getText().toString())) {
+            loginOk();
+        } else {
+            Log.i("CAMOTERO", "Contraseña incorrecta.");
+            Toast.makeText(MainActivity.this, "Correo y/o contraseña incorrecta!", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+
+    private void loginOk() {
+        // Se cambia de actividad.
+        Intent i = new Intent(MainActivity.this, NavigationActivity.class);
+        startActivity(i);
+
+        // Se destruye el activity actual.
+        finish();
+    }
+
+
 }
